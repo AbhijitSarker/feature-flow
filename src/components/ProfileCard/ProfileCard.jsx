e784a1621e9d1c3f4895a01c5ef282520a56ad2e
AbhijitSarker <abhijitsarker03@gmail.com>
2023-12-28 03:34:10 +0600

    added login system on profile card

--- src/components/ProfileCard/ProfileCard.jsx
+++ src/components/ProfileCard/ProfileCard.jsx
@@ -1,13 +1,22 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import useAuth from '../../hooks/useAuth';
 
 const ProfileCard = () => {
-    return (
+    const { logout, user } = useAuth();
+    const handleLogout = () => {
+        logout();
+    }; return (
         <div className="font-baskerville  border shadow-xl w-full flex flex-row justify-center items-center">
             <div className="card w-full mx-auto bg-white  hover:shadow-xl shadow">
                 <img className="w-32 mx-auto  rounded-full -mt-20 border-8 border-white" src="https://avatars.githubusercontent.com/u/67946056?v=4" alt="" />
-                <div className="text-center mt-2 text-3xl font-medium">Ajo Alex</div>
-                <div className="text-center mt-2 font-light text-sm">@devpenzil</div>
+                {
+                    user ? <div className="text-center mt-2 text-3xl font-medium">{user.displayName}</div>
+
+                        : <div className="text-center mt-2 text-3xl font-medium">Your Name</div>
+                }
+
+                <div className="text-center mt-2 font-light text-sm">Email: {user?.email}</div>
 
                 <hr className="mt-8" />
                 <div className="flex p-4">
@@ -22,7 +31,19 @@ const ProfileCard = () => {
                     </div>
                 </div>
                 <hr className="mb-8" />
-                <Link to={'/signin'}><div className=" mt-5 py-3 m-5  border text-center text-primary hover:text-secondary text-2xl font-semibold  rounded-lg transition duration-200 hover:bg-primary ease">Sign Up | Sign IN</div></Link>
+
+                {
+                    user ?
+                        <div onClick={handleLogout} className=" mt-5 py-3 m-5  border text-center text-primary hover:text-secondary text-2xl font-semibold  rounded-lg transition duration-200 hover:bg-primary ease">Logout</div>
+                        :
+                        <Link to={'/signin'}>
+                            <div className=" mt-5 py-3 m-5  border text-center text-primary hover:text-secondary text-2xl font-semibold  rounded-lg transition duration-200 hover:bg-primary ease">Sign Up | Sign IN</div>
+                        </Link>
+
+
+                }
+
+
             </div>
         </div>
     );
