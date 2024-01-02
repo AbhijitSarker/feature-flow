8e646b50c1a7a2ac89d316db089fd80cb7108887
AbhijitSarker <abhijitsarker03@gmail.com>
2024-01-02 18:57:29 +0600

    delete comment feature added

--- src/components/Comment/Comment.jsx
+++ src/components/Comment/Comment.jsx
@@ -1,22 +1,28 @@
 import React from 'react';
+import api from '../../utils/handleApi';
+import useComments from '../../hooks/useComments';
 
-const Comment = ({ comment }) => {
+const Comment = ({ comment, featureId }) => {
+    const { refetch } = useComments(featureId)
+    // console.log(comment)
     const handleDelete = (commentId) => {
-        // Handle comment deletion here
-        console.log(`Deleting comment with ID: ${commentId}`);
+        api.delete(`/comment/${commentId}`)
+            .then(() => {
+                refetch()
+            })
     };
     return (
         <div className="flex items-start mb-4">
             <img
                 src={comment.photoURL}
-                alt={`${comment.author}'s avatar`}
+                alt={`${comment.name}'s avatar`}
                 className="w-10 h-10 rounded-full object-cover mr-4"
             />
             <div className="flex-1">
                 <div className="flex items-center justify-between">
-                    <p className="text-sm font-semibold">{comment.author}</p>
+                    <p className="text-sm font-semibold">{comment.name}</p>
                     <button
-                        onClick={() => handleDelete(comment.id)}
+                        onClick={() => handleDelete(comment._id)}
                         className="text-red-500 hover:text-red-700 focus:outline-none"
                     >
                         Delete
