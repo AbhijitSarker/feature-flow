f6391daf78db5d8055b7874e290d72ebac221d9e
AbhijitSarker <abhijitsarker03@gmail.com>
2024-01-04 19:37:34 +0600

    delete feature functionality for feature table

--- src/components/FeatureTable/FeatureTable.jsx
+++ src/components/FeatureTable/FeatureTable.jsx
@@ -1,7 +1,31 @@
 import React from 'react';
+import Swal from 'sweetalert2'
+import api from '../../utils/handleApi';
 
 const FeatureTable = ({ filteredFeatures }) => {
     console.log(filteredFeatures)
+    const deleteFeature = (userId) => {
+        try {
+            Swal.fire({
+                title: "Are you sure?",
+                showCancelButton: true,
+                confirmButtonColor: "#3085d6",
+                cancelButtonColor: "#d33",
+                confirmButtonText: "Yes, Delete!"
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    api.delete(`/feature/${userId}`);
+
+                    Swal.fire({
+                        title: "Successful!",
+                    });
+                }
+            });
+
+        } catch (error) {
+            console.error('Error deleting user:', error);
+        }
+    };
     return (
         <div class="text-gray-900 bg-gray-200 ">
 
@@ -14,7 +38,7 @@ const FeatureTable = ({ filteredFeatures }) => {
                             <th class="text-left p-3 px-5">Likes </th>
                             <th class="text-left p-3 px-5"> Comments</th>
                             <th class="text-left p-3 px-5">Status</th>
-                            <th></th>
+                            <th>Actions</th>
                         </tr>
                         {
                             filteredFeatures?.map(feature => <tr class="border-b hover:bg-orange-100">
@@ -25,7 +49,7 @@ const FeatureTable = ({ filteredFeatures }) => {
                                 <td class="p-3 px-5"> {feature.status}</td>
                                 <td class="p-3 px-5 flex justify-end">
                                     <button type="button" class="mr-3 text-sm bg-blue-500 hover:bg-blue-700 text-white py-1 px-2 rounded focus:outline-none focus:shadow-outline">Save</button>
-                                    <button type="button" class="text-sm bg-red-500 hover:bg-red-700 text-white py-1 px-2 rounded focus:outline-none focus:shadow-outline">Delete</button>
+                                    <button type="button" onClick={() => deleteFeature(feature._id)} class="text-sm bg-red-500 hover:bg-red-700 text-white py-1 px-2 rounded focus:outline-none focus:shadow-outline">Delete</button>
                                 </td>
                             </tr>)
                         }
