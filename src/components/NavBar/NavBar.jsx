4fe0b77df4cc317011532e2f46d4e7d0ad870693
AbhijitSarker <abhijitsarker03@gmail.com>
2024-01-16 18:42:07 +0600

    nav bar updated

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -9,10 +9,13 @@ const NavBar = () => {
     const [open, setOpen] = useState(false);
     const { data } = useApp();
     return (
-        <nav className="bg-gray-900 w-full h-60">
-            <div className='flex justify-between  pt-5  mx-2 md:mx-10 '>
+        <nav className="bg-gray-900 w-full">
+            <div className='flex justify-center  gap-10 py-8 container mx-auto'>
 
-                <LogoTitle></LogoTitle>
+                {/* <LogoTitle></LogoTitle> */}
+                <div>
+                    <h1 className='text-secondary  text-3xl md:text-4xl font-baskerville font-bold '>{data?.appInfo[0].title}</h1>
+                </div>
                 {/*for small device */}
                 <ul className={`absolute z-10 w-full h-screen flex flex-col gap-10 justify-center items-center bg-secondary transform duration-500 ease-in-out ${open ? 'left-0 top-0' : '-top-[2000px]  left-0'} `}>
                     <Link to={'/'} className='text-lg font-lg font-sans text-white hover:text-secondary rounded-md px-1'>Home</Link>
@@ -20,14 +23,18 @@ const NavBar = () => {
 
                 </ul>
 
+
+
+                <div className='flex text-secondary'>
+                    <img className='w-10 h-10 rounded-md mr-3 ' src={data?.appInfo[0].logo} alt="" />
+                </div>
+
                 {/* for medium and large device */}
-                <ul className='md:flex hidden  space-x-5'>
+                <ul className='md:flex hidden items-center  space-x-5'>
                     <Link to={'/'} className='text-lg font-lg font-sans text-white hover:text-secondary rounded-md px-1'>Home</Link>
                     <Link to={'/dashboard'} className='text-lg font-lg font-sans text-white hover:text-secondary rounded-md px-1'>Dashboard</Link>
+                    <Link to={'/signin'} className='text-lg font-lg font-sans text-white hover:text-secondary rounded-md px-1'>Login</Link>
                 </ul>
-
-
-                <div></div>
                 {/* <Link to={'/signup'}><button className='hidden md:flex hover:text-secondary   w-36 h-12 items-center justify-center rounded-lg font-bold text-xl text-white transition ease-in-out duration-200'>Login / Register</button></Link> */}
                 <div className='md:hidden z-20' onClick={() => setOpen(!open)}>
                     {
@@ -35,11 +42,13 @@ const NavBar = () => {
                     }
                 </div>
             </div>
-            <div className='hidden md:flex justify-center container mx-auto w-[700px] mt-5 text-secondary'>
-                <p>{data?.appInfo[0].description}</p>
-            </div>
+
         </nav>
     );
 };
 
 export default NavBar;
+
+{/* <div className='hidden md:flex justify-center container mx-auto w-[700px] mt-5 text-secondary'>
+    <p>{data?.appInfo[0].description}</p>
+</div> */}
--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -25,37 +25,45 @@ const Main = () => {
                 <title>Home | Feature Flow </title>
             </Helmet>
             <Navbar></Navbar>
-            <div className="rounded  flex flex-col lg:flex-row mx-2 md:mx-14 p-3 bg-white shadow-md -mt-40 md:-mt-16">
 
-                {/* Mobile Menu Icon */}
-                <div className="lg:hidden flex justify-between items-center">
-                    <button onClick={toggleSidebar} className="text-gray-200 p-3 focus:outline-none" >
-                        {isSidebarOpen ? <GiCrossedBones className='text-4xl text-primary' /> : <FaBarsStaggered className='text-4xl text-primary' />} </button>
-                    <div className='text-primary text-2xl font-semibold flex items-center'>
-                        <FaArrowLeft />
-                        <span>Request Feature</span>
-                    </div>
-                </div>
 
-                {/* Sidebar */}
-                <aside className={` rounded-md space-y-16   md:h-full text-primary w-full lg:w-1/3 ${isSidebarOpen ? 'block' : 'hidden'} lg:block  p-3 md:p-3`} >
-                    {/* Sidebar content */}
-                    <ProfileCard></ProfileCard>
-                    {/* <h1 className='text-primary text-4xl text-center mb-5 font-bold  '>Feature Flow</h1> */}
-                    <FeatureForm></FeatureForm>
 
-                </aside>
 
-                {/* Main content area */}
-                <main className="flex-1 px-2 md:pl-5 md:pr-2">
-                    {/* Main content */}
-                    <Outlet></Outlet>
 
-                </main>
-            </div>
+
+
+
+
+
+
+
             <Footer></Footer>
         </div >
     );
 };
 
 export default Main;
+
+
+// <div className="rounded  flex flex-col lg:flex-row mx-2 md:mx-14 p-3 bg-white shadow-md -mt-40 md:-mt-16">
+
+//     <div className="lg:hidden flex justify-between items-center">
+//         <button onClick={toggleSidebar} className="text-gray-200 p-3 focus:outline-none" >
+//             {isSidebarOpen ? <GiCrossedBones className='text-4xl text-primary' /> : <FaBarsStaggered className='text-4xl text-primary' />} </button>
+//         <div className='text-primary text-2xl font-semibold flex items-center'>
+//             <FaArrowLeft />
+//             <span>Request Feature</span>
+//         </div>
+//     </div>
+
+//     <aside className={` rounded-md space-y-16   md:h-full text-primary w-full lg:w-1/3 ${isSidebarOpen ? 'block' : 'hidden'} lg:block  p-3 md:p-3`} >
+//         <ProfileCard></ProfileCard>
+//         <FeatureForm></FeatureForm>
+
+//     </aside>
+
+//     <main className="flex-1 px-2 md:pl-5 md:pr-2">
+//         {/*  content */}
+//         <Outlet></Outlet>
+//     </main>
+// </div>
